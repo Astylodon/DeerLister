d1355d3494b0f719d01c6546b6cd4582962b334f
Xwilarg <xwilarg@protonmail.com>
2022-09-15 22:05:47 +0200

    Improve documentation

--- _internal/displays/FileDisplay.php
+++ _internal/displays/FileDisplay.php
@@ -2,5 +2,12 @@
 
 interface FileDisplay
 {
+    /**
+     * Whether the current file display handles this file
+     *
+     * @param string $ext The file extension without leading dot
+     *
+     * @return bool Whether the file preview handles the file
+     */
     public function doesHandle(string $ext): bool;
 }
