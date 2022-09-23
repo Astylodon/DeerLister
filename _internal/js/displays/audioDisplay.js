944080c26cc1844c2d5438daaae65ee516335a71
Xwilarg <xwilarg@protonmail.com>
2022-09-24 01:25:50 +0200

    Rename variable

--- _internal/js/displays/audioDisplay.js
+++ _internal/js/displays/audioDisplay.js
@@ -1,12 +1,13 @@
-const musics = Array.from(document.getElementsByClassName("audio-preview"));
+const songs = Array.from(document.getElementsByClassName("audio-preview"));
 const player = document.getElementById("audio-player");
 player.volume = 0.25;
-if (musics.length > 0)
+if (songs.length > 0)
 {
-    player.src = musics[0];
+    player.src = songs[0];
 }
-for (const a of musics) {
+for (const a of songs) {
     const href = a.href;
+    const icon = a.getElementsByClassName("icon")[0]
     a.addEventListener("click", e => {
         e.preventDefault();
         player.src = href;
