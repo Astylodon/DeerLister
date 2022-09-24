fd0804c972171981b5e89d270dfad59ddf421639
Xwilarg <xwilarg@protonmail.com>
2022-09-24 11:05:18 +0200

    Play next song on end

--- _internal/js/displays/audioDisplay.js
+++ _internal/js/displays/audioDisplay.js
@@ -1,3 +1,5 @@
+var audioIndex = 0;
+
 const songs = Array.from(document.getElementsByClassName("audio-preview"));
 const player = document.getElementById("audio-player");
 player.volume = 0.25;
@@ -7,10 +9,16 @@ if (songs.length > 0)
 }
 for (const a of songs) {
     const href = a.href;
-    const icon = a.getElementsByClassName("icon")[0]
     a.addEventListener("click", e => {
         e.preventDefault();
         player.src = href;
         player.play();
     });
 }
+player.addEventListener("ended", () => {
+    if (audioIndex < songs.length - 1) {
+        audioIndex++;
+        player.src = songs[audioIndex].href;
+        player.play();
+    }
+});
