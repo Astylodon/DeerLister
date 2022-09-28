be6e7d0d207a440a75e26102cc373145dccfd341
Xwilarg <xwilarg@protonmail.com>
2022-09-28 13:49:12 +0200

    Fix filename display

--- _internal/js/displays/audioDisplay.js
+++ _internal/js/displays/audioDisplay.js
@@ -6,13 +6,13 @@ player.volume = 0.25;
 if (songs.length > 0)
 {
     player.src = songs[0].href;
-    document.getElementById("audio-title").innerHTML = songs[0].outerText;
+    document.getElementById("audio-title").innerHTML = songs[0].dataset.filename;
 }
 for (const a of songs) {
     a.addEventListener("click", e => {
         e.preventDefault();
         player.src = a.href;
-        document.getElementById("audio-title").innerHTML = a.outerText;
+        document.getElementById("audio-title").innerHTML = a.dataset.filename;
         player.play();
     });
 }
@@ -20,7 +20,7 @@ player.addEventListener("ended", () => {
     if (audioIndex < songs.length - 1) {
         audioIndex++;
         player.src = songs[audioIndex].href;
-        document.getElementById("audio-title").innerHTML = songs[audioIndex].outerText;
+        document.getElementById("audio-title").innerHTML = songs[audioIndex].dataset.filename;
         player.play();
     }
 });
