472afe4022f4a89d6efc05108c5b7d99c64fc292
Indra <wuppie55@gmail.com>
2022-10-29 18:31:45 +0200

    Fix audioDisplay.js code style

--- _internal/js/displays/audioDisplay.js
+++ _internal/js/displays/audioDisplay.js
@@ -1,13 +1,15 @@
-var audioIndex = 0;
+let audioIndex = 0;
 
 const songs = Array.from(document.getElementsByClassName("audio-preview"));
 const player = document.getElementById("audio-player");
+
 player.volume = 0.25;
-if (songs.length > 0)
-{
+
+if (songs.length > 0) {
     player.src = songs[0].href;
     document.getElementById("audio-title").innerHTML = songs[0].dataset.filename;
 }
+
 for (const a of songs) {
     a.addEventListener("click", e => {
         e.preventDefault();
@@ -16,6 +18,7 @@ for (const a of songs) {
         player.play();
     });
 }
+
 player.addEventListener("ended", () => {
     if (audioIndex < songs.length - 1) {
         audioIndex++;
